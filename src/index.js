2283234e1d031888c1d1ae3845c7917cb5acac81
v_sapovsky <vadim.sapovsky@ahoii.net>
2019-02-06 19:38:42 +0200

    react setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('My Minimal React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Sound App';
+
+ReactDOM.render(
+    <div>{ title }</div>,
+    document.getElementById('app')
+);
+
+module.hot.accept();
