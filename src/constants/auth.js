db9c3d24e9213a4effe28b641e81deccfd15b1bc
v_sapovsky <vadim.sapovsky@ahoii.net>
2019-02-07 17:43:31 +0200

    new app registration

---
+++ src/constants/auth.js
@@ -0,0 +1,3 @@
+export const CLIENT_ID = '36fa98c41d954e728c5aaec918ae6b41';
+export const REDIRECT_URI = `${window.location.protocol}//${window.location.host}/callback`;
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import SpotifyWebApi from 'spotify-web-api-node';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
 import * as actions from './actions';
-
 import Stream from './components/Stream';
+import { CLIENT_ID, REDIRECT_URI } from "./constants/auth";
+
+window.spotifyApi = new SpotifyWebApi({
+    clientId: CLIENT_ID,
+    redirectUri: REDIRECT_URI
+});
 
 const tracks = [
     {
