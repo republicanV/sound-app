6be87e4d0947dfd3bcbcd765002c3e70d2db49e0
v_sapovsky <vadim.sapovsky@ahoii.net>
2019-02-07 11:38:04 +0200

    add stream component

---
+++ src/components/Stream.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Stream({ tracks = [] }) {
+
+    return (
+        <div>
+            {
+                tracks.map((track, key) => {
+                    return <div className="track" key={key}>{ track.title }</div>;
+                })
+            }
+        </div>
+    );
+}
+
+export default Stream;
